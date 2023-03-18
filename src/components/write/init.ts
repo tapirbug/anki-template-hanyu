6f7e03b725e36f9794d19900ca91cb2ff1f2197e
krachzack <hello@phstadler.com>
2023-03-18 14:48:36 +0100

    chore: fix lints in components/write/init.ts

--- src/components/write/init.ts
+++ src/components/write/init.ts
@@ -4,8 +4,10 @@ import { createWriter } from './write'
 const writerClass = 'strichfolge-animation'
 
 /// Initializes all hanzi writers in the current document
-export function init () {
-  for (const writerContainer of document.getElementsByClassName(writerClass)) {
+export function init (): void {
+  const writerContainers = document.getElementsByClassName(writerClass)
+  for (let i = 0; i < writerContainers.length; ++i) {
+    const writerContainer = writerContainers[i]
     if (!writerContainer.classList.contains('is-initialized')) {
       const text = writerContainer.textContent?.trim()
       if (text) {
