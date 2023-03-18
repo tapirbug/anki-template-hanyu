279b6b4e413fae84007dc6e0fd62c69e73b80186
krachzack <hello@phstadler.com>
2023-03-18 15:05:54 +0100

    chore: fix lint in src/components/debug/init.ts

--- src/components/debug/index.ts
+++ src/components/debug/index.ts
@@ -4,11 +4,26 @@ import { renderAnkiTagsInElement } from './anki-tags'
 export * from './log'
 
 // show severe errors in anki so that we know that they happen
-window.onerror = (evtOrMsgString, source, lineNo) => { error(`Error: "${evtOrMsgString.toString()}" at ${source}:${lineNo}`) }
+window.onerror = (evtOrMsgString, source, lineNo, _col, errorObj) => {
+  let msg = 'Error: '
+  if (typeof errorObj !== 'undefined') {
+    msg += `"${msg}"`
+  } else if (typeof evtOrMsgString === 'string') {
+    msg += `"${msg}"`
+  } else {
+    msg += '<no message>'
+  }
+  if (typeof source !== 'undefined') {
+    msg += ` at ${source}`
+    if (typeof lineNo !== 'undefined') {
+      msg += `:${lineNo}`
+    }
+  }
+  error(msg)
+}
 
 // render anki tags in dev mode once when everything appears to have loaded
 // (no effect in actual anki)
 if (process.env.NODE_ENV !== 'production') {
-  // window.addEventListener('DOMContentLoaded', () => renderAnkiTagsInElement(document.documentElement))
   renderAnkiTagsInElement(document.documentElement)
 }
